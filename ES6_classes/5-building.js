795b395841e3ac460ece70476130c9733d32ca99
GenXwater <louis.genty@outlook.com>
2024-11-15 16:51:34 +0100

    added task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,11 +3,11 @@ export default class Building {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage')
     }
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   get sqft() {
-    return this.sqft;
+    return this._sqft;
   }
 
   set sqft(value) {
