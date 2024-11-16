d9b39b34a39fc6b3db0eeedf52bed9c407d15a76
GenXwater <louis.genty@outlook.com>
2024-11-16 23:48:07 +0100

    fix codding style

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,18 +1,40 @@
+/* eslint-disable */
 export default class Airport {
   constructor(name, code) {
-    this._name = name;
-    this._code = code;
+      if (typeof name !== 'string') {
+          throw new TypeError('Name must be a string');
+      }
+      if (typeof code !== 'string') {
+          throw new TypeError('Code must be a string');
+      }
+
+      this._name = name;
+      this._code = code;
   }
 
   get name() {
-    return this._name;
+      return this._name;
+  }
+
+  set name(value) {
+      if (typeof value !== 'string') {
+          throw new TypeError('Name must be a string');
+      }
+      this._name = value;
   }
 
   get code() {
-    return this._code;
+      return this._code;
+  }
+
+  set code(value) {
+      if (typeof value !== 'string') {
+          throw new TypeError('Code must be a string');
+      }
+      this._code = value;
   }
 
   toString() {
-    return `[objet ${this._code}]`;
+      return `[object ${this._code}]`;
   }
 }
