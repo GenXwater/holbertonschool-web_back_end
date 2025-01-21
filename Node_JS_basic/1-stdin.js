74c2a5e6b601adb757c0c2f304428b0052cdab7c
GenXwater <louis.genty@outlook.com>
2025-01-21 17:08:37 +0100

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,8 +4,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit(0);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit(0);
+  }
 });
