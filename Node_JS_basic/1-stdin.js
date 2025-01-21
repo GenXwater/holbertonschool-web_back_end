53fc39830a434eca6d06b2db08fb9b49107eae68
GenXwater <louis.genty@outlook.com>
2025-01-21 17:01:04 +0100

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
+// program for display the message.
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is ${name}`);
+  process.stdout.write(`Your name is: ${name}`);
   process.stdout.write('This important software is now closing\n');
   process.exit(0);
 });
