49c8e73807766c5a2d7788acb2cc2b9504855c77
GenXwater <louis.genty@outlook.com>
2025-01-21 11:25:03 +0100

    for test checker

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 // Ce programme fonctionne parfaitement, pourquoi le checker est si dur avec moi
 function displayMessage(message) {
-  process.stdout.write(`${message}\n`);
+  console.log(message);
 }
 
 function exitProgramme() {
