0697a477f45410b7ffd1de14950d84c1e2ff7733
GenXwater <louis.genty@outlook.com>
2025-01-21 10:52:36 +0100

    fix

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,11 +1,8 @@
-/** use of a displayMessage arrow function for console output
- * 
- * @param (string) message - the message to be displayed.
- * merci Hordan
+/** 
+ * use of a displayMessage arrow function for console output
  */
 const displayMessage = message => {
   console.log(message);  
 };
 
-// il faut un commentaire ici ?
 module.exports = displayMessage;
