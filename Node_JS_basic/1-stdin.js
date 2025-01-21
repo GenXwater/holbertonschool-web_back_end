2b3c865740f9723405c7f39e19fa45c1dee062a1
GenXwater <louis.genty@outlook.com>
2025-01-21 10:24:58 +0100

    ok

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+function displayMessage(message) {
+  process.stdout.write(message + "\n");
+};
+
+function inputName() {
+  process.stdin.setEncoding('utf8'); // l'entrée est traitée en tant que chaîne de caractères
+  process.stdin.on('data', (chunk) => {
+    const name = chunk.trim(); // trim supprime les espaces superflus
+    console.log(`Your name is: ${name}`);
+    exitProgramme();
+  })
+};
+
+function exitProgramme() {
+  console.log('This important software is now closing');
+  process.exit();
+}
+
+displayMessage("Welcome to Holberton School, what is your name?");
+inputName();
