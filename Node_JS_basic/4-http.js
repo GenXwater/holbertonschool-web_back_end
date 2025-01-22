06280993f4850c16328d9db00e4a8fab840bb17e
GenXwater <louis.genty@outlook.com>
2025-01-22 17:22:32 +0100

    added

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
+// created server node ok
+
 const http = require('http');
 
 // Creat server :
 const app = http.createServer((_, res) => {
-  res.statusCode = 200;
-  res.setHeader(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Holberton School');
 });
 
