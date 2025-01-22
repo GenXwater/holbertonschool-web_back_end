92d6439e6d60372939328bb1c8a218131a417195
GenXwater <louis.genty@outlook.com>
2025-01-22 12:20:12 +0100

    added task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// Creat server :
+const app = http.createServer((_, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Holberton School');
+});
+
+// Le serveur écoute sur le port 1245 :
+app.listen(1245);
+
+module.exports = app;
