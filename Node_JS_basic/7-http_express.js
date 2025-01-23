4ea38e7329938d248c9b8409bb146582f99432eb
GenXwater <louis.genty@outlook.com>
2025-01-23 16:59:28 +0100

    added task 7

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');    
+});
+  
+app.get('/students', (req, res) => {
+  const database = process.argv[2];
+  countStudents(database)
+    .then((output) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
